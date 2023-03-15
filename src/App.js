b5a747e16e50eb21ec214290b340dc0ad72ea821
zubxxr <zubxxr@gmail.com>
2023-03-15 13:29:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Blackjack from './Blackjack';
+// import Test from './Test';
+import Blackjack2 from './Blackjack2';
+
+
 
 function App() {
+
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Blackjack />
+      {/* <Blackjack2 /> */}
+
+      {/* <Test /> */}
+
+      
+    </>
   );
 }
 
---
+++ src/Test.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+import "./Test.css"
+
+const Test = () => {
+
+    let arr = []
+    function joe(x) {
+        
+        let y = String(x)
+        for (let i = 0; i < y.length; i++) {
+            arr.push[y[i]]
+        }
+    };
+
+    return (
+        <>
+           
+            {console.log(joe(121))}
+
+
+        </>
+    )
+
+}
+export default Test
